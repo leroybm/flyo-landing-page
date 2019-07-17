4ce9050f8efa33f1c9433485460e2ce815ff12f2
Leroy Medeiros <lelo.drax@gmail.com>
2019-07-16 22:24:11 -0300

    feat: migrates layout to handlebars

--- webpack.config.js
+++ webpack.config.js
@@ -2,45 +2,48 @@ const path = require('path');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(s*)css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-          'postcss-loader'
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/,
-        use: [
-          'file-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new StyleLintPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      filename: path.resolve(__dirname, 'index.html'),
-      alwaysWriteToDisk: true
-    }),
-    new HtmlWebpackHarddiskPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+	mode: 'development',
+	entry: './src/js/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'dist')
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		hot: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(s*)css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+					'postcss-loader'
+				]
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.handlebars$/,
+				use: [
+					'handlebars-loader'
+				]
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new StyleLintPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin()
+	]
 };
