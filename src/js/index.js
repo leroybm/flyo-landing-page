085a6818ce7fe0dfbd55598916f01f535e672537
Leroy Medeiros <lelo.drax@gmail.com>
2019-09-25 10:07:36 -0300

    feat: desktop layout (#2)
    * feat: desktop header layout
    
    * feat: desktop intro layout
    
    * feat: desktop features layout
    
    * feat: desktop stay-productive layout part 1
    
    * feat: desktop stay-productive layout part 2
    
    * feat: migrates layout to handlebars
    
    * feat: separate layout from data
    
    * feat: testimonials and css order
    
    * feat: newsletter
    
    * fix: security issues and comment useless code
    
    * feat: newsletter (adding missing files)
    
    * feat: footer
    
    * fix: change distribution folder to public
    
    * fix: images not working in prod
    
    * fix: delete unecessary file

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,21 @@
-import '../css/main.scss';
+import '../css/main.scss'
+import staticData from '../data/static.json'
+import mainTemplate from '../views/main.hbs'
+
+/**
+ * Injects a handlebar template to the page
+ *
+ * @param { Handlebars } template - A handlebars template file
+ * @param { Object } data - Data to be passed to handlerbar template
+ */
+function injectTemplate(template, data = {}) {
+	if (!template) {
+		return;
+	}
+
+	const div = document.createElement('div')
+	div.innerHTML = template(data)
+	document.body.appendChild(div)
+}
+
+document.addEventListener('DOMContentLoaded', () => injectTemplate(mainTemplate, staticData))
--- webpack.config.js
+++ webpack.config.js
@@ -2,39 +2,59 @@ const path = require('path');
 const StyleLintPlugin = require('stylelint-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
-  entry: './src/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  devServer: {
-    hot: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(s*)css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-          'postcss-loader'
-        ]
-      }
-    ]
-  },
-  plugins: [
-    new StyleLintPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'src/index.html',
-      filename: path.resolve(__dirname, 'index.html'),
-      alwaysWriteToDisk: true
-    }),
-    new HtmlWebpackHarddiskPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ]
+	mode: 'development',
+	entry: './src/js/index.js',
+	output: {
+		filename: 'bundle.js',
+		path: path.resolve(__dirname, 'public')
+	},
+	devtool: 'inline-source-map',
+	devServer: {
+		hot: true
+	},
+	module: {
+		rules: [
+			{
+				test: /\.(s*)css$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+					'postcss-loader'
+				]
+			},
+			{
+				test: /\.(png|jpg|gif|svg)$/,
+				use: [
+					'file-loader'
+				]
+			},
+			{
+				test: /\.hbs$/,
+				loader: 'handlebars-loader',
+				options: {
+					inlineRequires: '\/img\/'
+				},
+			}
+		]
+	},
+	plugins: [
+		new CleanWebpackPlugin(),
+		new StyleLintPlugin(),
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			title: 'Frontend Mentor | Fylo landing page with dark theme and features grid',
+			meta: {
+				viewport: 'width=device-width, initial-scale=1.0',
+				charset: 'UTF-8'
+			}
+		}),
+		new CopyPlugin([
+			{ from: 'src/img', to: 'img' },
+		]),
+	]
 };
